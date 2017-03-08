7b530d41522d58fab524978cdc9bbe5b6ec47be2
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-08 19:43:47 -0300

    toggle

--- Turma B/Aula-03/programa.js
+++ Turma B/Aula-03/programa.js
@@ -1,6 +1,11 @@
 function ligar(){
   var b = document.querySelector("body");
-  b.classList.add("ligado");
+  /*if(b.classList.contains("ligado")){
+    b.classList.remove("ligado")
+  } else {
+    b.classList.add("ligado");
+  }*/
+  b.classList.toggle("ligado");
 }
 
 window.addEventListener("click",ligar);
