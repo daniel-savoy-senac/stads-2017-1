c2fb948e0028b23e47378038524510dbe3bc1f48
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-15 21:36:46 -0300

    arvore

--- Turma B/Aula-04/pessoa.js
+++ Turma B/Aula-04/pessoa.js
@@ -1,13 +1,28 @@
 function Pessoa(nome, sobrenome, idade, cpf, rg){
+  // PROPRIEDADES
   this.nome = nome;
   this.sobrenome = sobrenome;
   this.idade = idade;
   this.cpf = cpf;
   this.rg = rg || 0;
+  this.filhos = [];
+  // METODOS
   this.crescer = function(){
     return ++this.idade;
   }
+  this.adotar = function(filho){
+    this.filhos.push(filho);
+  }
+  //CONSTRUTOR
 }
 
-var daniel = new Pessoa("Daniel","Savoy",36,999);
-console.log(daniel);
+var joao = new Pessoa("João");
+var maria = new Pessoa("Maria");
+var julia = new Pessoa("Júlia");
+var victor = new Pessoa("Victor");
+var ze = new Pessoa("Zé");
+
+joao.adotar(maria);
+joao.adotar(julia);
+julia.adotar(victor);
+julia.adotar(ze);
