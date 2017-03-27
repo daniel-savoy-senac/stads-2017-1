2212a755e7579eff10b3ffae255d579cfaa75f76
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-27 20:18:15 -0300

    seletores

--- Turma A/Aula-06/dom.js
+++ Turma A/Aula-06/dom.js
@@ -1,5 +1,16 @@
 function iniciar(){
   //Ver se o botão foi clicado
+  /*
+    document.querySelector => Seleciona 1 usando CSS selector
+    document.querySelectorAll => Seleciona vários
+    document.getElementById => Seleciona 1 pelo ID
+    document.getElementsByClassName => Seleciona vários por 1 classe
+    document.getElementsByTagName => Seleciona vários pelo tipo da tag
+  */
+
+  var btn = document.getElementsByTagName("button")[0];
+  var btn = document.querySelector("button");
+  var btn = document.querySelectorAll("button")[0];
 }
 
 function validar(){
