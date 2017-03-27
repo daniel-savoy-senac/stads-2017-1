dee7632ba1ce34540b8b73fb93a8d4ef3ba24965
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-27 20:12:00 -0300

    dom 01

---
+++ Turma A/Aula-06/dom.js
@@ -0,0 +1,12 @@
+function iniciar(){
+  //Ver se o botão foi clicado
+}
+
+function validar(){
+  //Imprimir o valor do campo
+}
+
+
+
+//WINDOW => Aba do Browser
+window.addEventListener("load", iniciar);
