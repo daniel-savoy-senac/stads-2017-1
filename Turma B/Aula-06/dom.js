d7fa097d578563713436357961e225a08e6c99a9
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-29 20:07:42 -0300

    comandos de busca

--- Turma B/Aula-06/dom.js
+++ Turma B/Aula-06/dom.js
@@ -2,10 +2,14 @@
   //document.children[0].children[1].children[1];
   //document.querySelector("SELETOR CSS");
   //document.querySelectorAll("SELETOR CSS");
+  //document.getElementsByTagName("COMANDO HTML");
+  //document.getElementsByClassName("CLASSE");
+  //document.getElementById("ID");
+  //document.getElementsByName("NOME DO CAMPO");
+  //$x("X-PATH");
 
 function main(){
 
-    document.querySelector()
 }
 
 window.addEventListener("load",main);
