47d4b9d531431247b272d7171c7fcac6ab6a543e
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-29 19:59:12 -0300

    seletores

---
+++ Turma B/Aula-06/dom.js
@@ -0,0 +1,11 @@
+/* COMMANDOS DE BUSCA NO DOM*/
+  //document.children[0].children[1].children[1];
+  //document.querySelector("SELETOR CSS");
+  //document.querySelectorAll("SELETOR CSS");
+
+function main(){
+
+    document.querySelector()
+}
+
+window.addEventListener("load",main);
