47f3f30617191031c68ab13ae9c652052c566bbf
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-03-29 21:25:38 -0300

    comandos

--- Turma B/Aula-06/dom.js
+++ Turma B/Aula-06/dom.js
@@ -1,13 +1,26 @@
 /* COMMANDOS DE BUSCA NO DOM*/
-  //document.querySelector("SELETOR CSS");
-  //document.querySelectorAll("SELETOR CSS");
+  // document.querySelector("SELETOR CSS");
+  // document.querySelectorAll("SELETOR CSS");
 
-  //document.children[0].children[1].children[1];
-  //document.getElementsByTagName("COMANDO HTML");
-  //document.getElementsByClassName("CLASSE");
-  //document.getElementById("ID");
-  //document.getElementsByName("NOME DO CAMPO");
-  //$x("X-PATH");
+  // document.children[0].children[1].children[1];
+  // document.getElementsByTagName("COMANDO HTML");
+  // document.getElementsByClassName("CLASSE");
+  // document.getElementById("ID");
+  // document.getElementsByName("NOME DO CAMPO");
+  // $x("X-PATH");
+
+
+/* COMANDOS COM ELEMENTOS */
+  // elemento.parentNode ==> Acessa o pai do elemento
+  // document.createElement("COMANDO HTML");
+  // pai.appendChild(filho);
+  // elemento.remove(); //CUIDADO NO IE;
+  // elemento.parentNode.removeChild(elmento);
+
+/* COMANDO DE MODIFICAÇÃO */
+  // elemtno.setAttribute("ATTR","VALOR");
+  // elemento.PROPRIEDADE = VALOR;
+  // elemento.style["ESTILO CSS"] = VALOR;
 
 function main(){
   var btn = document.querySelector("button#btn");
