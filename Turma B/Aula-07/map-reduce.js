c790c989b795868b3abf46b45224838a85dc6885
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-04-19 22:10:37 -0300

    comandos

--- Turma B/Aula-07/map-reduce.js
+++ Turma B/Aula-07/map-reduce.js
@@ -1,4 +1,11 @@
-var valores = [0,1,2,3,4,5,6,7,8,9];
+var valores = [0,1,2,3,4,5,6,7,8,9,11];
+
+var pessoas = [
+  {"nome":"Daniel", "idade":36},
+  {"nome":"Victor", "idade":23},
+  {"nome":"Silvia", "idade":43},
+  {"nome":"Heloiza", "idade":12}
+];
 
 
 var print = (v) => {
@@ -7,4 +14,23 @@ var print = (v) => {
   document.body.appendChild(e);
 }
 
-print("Soma:"+valores.reduce((a,n)=>a+n));
+print("Idade Média:"+pessoas.reduce((a,n)=>a+n.idade,0)/pessoas.length);
+print("Média:"+valores.reduce((a,n)=>a+n)/valores.length);
+print("Tem maior que 10?:"+valores.some(n=>n>10));
+print("TODOS maiores que -1?:"+valores.every(n=>n>-1));
+
+
+
+
+//valores.map(n => n*n).forEach(n => print(n));
+
+/*
+
+  MAP - cria um vetor a partir de outro vetor
+  REDUCE - cria um único valor a partir de um vetor
+  FOREACH - percorre o vetor
+  JOIN - transforma o vetor em uma string (inverso é SPLIT)
+  EVERY - se todo o vetor cumpre uma condição
+  SOME - se alguém cumpre uma condição
+
+*/
