cfef5f76ab9edafef6949ee3fece949c6191910e
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-04-24 21:36:50 -0300

    map reduce e outras funções de vetores

--- Turma A/Aula-08/programa.js
+++ Turma A/Aula-08/programa.js
@@ -8,6 +8,20 @@ let iniciar = (e) => {
   xhr.open("get", "dados.json");
   xhr.addEventListener("load", atualizarLista);
   xhr.send();
+  document.querySelector("#proximo").addEventListener("click",irProximo);
+  document.querySelector("#anterior").addEventListener("click",irAnterior);
+}
+
+let irProximo = () => {
+  ++atual;
+  if(atual >= lista.length) atual = 0;
+  imprimir(lista[atual]);
+}
+
+let irAnterior = () => {
+  --atual;
+  if(atual < 0) atual = lista.length-1;
+  imprimir(lista[atual]);
 }
 
 let atualizarLista = (evt) => {
@@ -25,3 +39,18 @@ let imprimir = (obj) => {
 }
 
 window.addEventListener("load", iniciar);
+
+
+/*
+
+  MAP - cria um vetor a partir de outro vetor
+  REDUCE - cria um único valor a partir de um vetor
+  FOREACH - percorre o vetor
+  JOIN - transforma o vetor em uma string (inverso é SPLIT)
+  EVERY - se todo o vetor cumpre uma condição
+  SOME - se alguém cumpre uma condição
+  FILTER - retorna um vetor com apenas os valores que cumpram a condição
+  SORT - orderna o vetor de acordo com a função
+  REVERSE - retorna o vetor na ordem reversa
+
+*/
