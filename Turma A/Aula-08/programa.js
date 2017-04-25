0a9bb60f9c5ed5437a2b29675c42cb9f99287d27
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-04-24 21:15:17 -0300

    js

--- Turma A/Aula-08/programa.js
+++ Turma A/Aula-08/programa.js
@@ -1,14 +1,23 @@
-var xhr = new XMLHttpRequest();
+let lista = [];
+let atual = 0;
+
 
 let iniciar = (e) => {
   // Requisicao por XHR
+  let xhr = new XMLHttpRequest();
   xhr.open("get", "dados.json");
-  xhr.addEventListener("load",imprimir);
+  xhr.addEventListener("load", atualizarLista);
   xhr.send();
 }
 
-let imprimir = () => {
-  let obj = JSON.parse(xhr.response);
+let atualizarLista = (evt) => {
+  let res = evt.target.response;
+  lista = JSON.parse(res);
+  atual = 0;
+  imprimir(lista[atual]);
+}
+
+let imprimir = (obj) => {
   for(let prop in obj){
     let campo = document.querySelector(`#${prop}`);
     campo.value = obj[prop];
