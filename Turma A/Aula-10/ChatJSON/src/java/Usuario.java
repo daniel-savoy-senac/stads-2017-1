1655b9097f87876fe7a4b7235d1649d92c874b9c
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-05-15 20:12:57 -0300

    form login

---
+++ Turma A/Aula-10/ChatJSON/src/java/Usuario.java
@@ -0,0 +1,40 @@
+public class Usuario {
+
+    protected String username;
+    
+    protected String imageUrl;
+    
+    protected String status;
+    
+    public Usuario(String username, String imageUrl){
+        this.username = username;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+    
+}
