435c0d5dc73183770efb183c589eaf22d698878c
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-05-17 19:40:05 -0300

    mensagem

--- Turma B/Aula-10/ChatJSON/src/java/Mensagem.java
+++ Turma B/Aula-10/ChatJSON/src/java/Mensagem.java
@@ -1,13 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author daniel.psavoy
- */
+import java.util.Date;
+
 public class Mensagem {
+    
+    protected String text;
+    
+    protected Date time;
+    
+    protected Usuario usuario;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Mensagem(String text, Usuario usuario) {
+        this.time = new Date();
+        this.text = text;
+        this.usuario = usuario;
+    }
+    
+    
     
 }
