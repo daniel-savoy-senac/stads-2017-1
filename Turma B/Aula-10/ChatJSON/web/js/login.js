ddaddac71b8ea8b99ce52e3a0b57340bd67f7240
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-05-17 21:27:19 -0300

    resposta

---
+++ Turma B/Aula-10/ChatJSON/web/js/login.js
@@ -0,0 +1,30 @@
+function iniciar(){
+    document.querySelector("#loginBtn")
+            .addEventListener("click", submit);
+}
+
+function submit(){
+    let form = document.querySelector("form#loginForm");
+    let data = FormData2Str(new FormData(form));
+    let param = {
+        body:data,
+        credentials: "same-origin",
+        method: "POST",
+        headers: {
+             "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+         }
+    };
+    
+    fetch("Autenticar",param)
+            .then(r => r.json())
+            .then(json => console.log(json));
+       
+}
+
+function FormData2Str(fd){
+    let vector = Array.from(fd);
+    let text = vector.map(a => a[0]+"="+a[1]).join("&");
+    return encodeURI(text);
+}
+
+window.addEventListener("load",iniciar);
