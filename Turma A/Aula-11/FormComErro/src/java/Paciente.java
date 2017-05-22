3b18b1326e43d5a8fec7bde2a6f1e6188f5e5325
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-05-22 19:39:54 -0300

    paciente

---
+++ Turma A/Aula-11/FormComErro/src/java/Paciente.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daniel.psavoy
+ */
+public class Paciente {
+    
+    protected String nome;
+    protected float peso;
+    protected int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Paciente(String nome, float peso, int idade) {
+        this.nome = nome;
+        this.peso = peso;
+        this.idade = idade;
+    }
+    
+    
+    
+}
